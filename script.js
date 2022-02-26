ba811bce3758e95be5b2609e095449ef999afe98
Emmanuel Payares <emmanuelpayares.idetp@gmail.com>
2022-02-25 19:14:43 -0500

    gameplay incomplete

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function computerPlay() {
+    let r;
     let a = Math.floor(Math.random() * 3);
-    return a;
+    if (a == 0) {
+        r = "piedra";
+    } else if (a == 1) {
+        r = "papel";
+    } else {
+        r = "tijera";
+    }
+    return r;
 }
 
+function gameplay() {
+    let answer = prompt("Piedra, papel o tijera!");
+    
+    if (answerReal == "") {
+
+    }
+}

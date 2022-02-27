4032c4abe0ff82d3b3037f9def8ba9e17ac02a1d
Emmanuel Payares <emmanuelpayares.idetp@gmail.com>
2022-02-27 17:20:32 -0500

    solution

--- script.js
+++ script.js
@@ -1,20 +1,54 @@
 function computerPlay() {
-    let r;
-    let a = Math.floor(Math.random() * 3);
+    let a;
+    a = Math.floor(Math.random() * 3);
     if (a == 0) {
-        r = "piedra";
+        return "piedra";
     } else if (a == 1) {
-        r = "papel";
+        return "papel"
     } else {
-        r = "tijera";
-    }
-    return r;
-}
-
-function gameplay() {
-    let answer = prompt("Piedra, papel o tijera!");
-    
-    if (answerReal == "") {
-
+        return "tijera";
     }
 }
+
+
+function playerSelection() {
+    let a = prompt("Digita piedra, papel o tijera!");
+    a = a.toLowerCase();
+    return a;
+}
+
+
+function gameplay(playerSelection, computerSelection) {
+    console.log("La maquina tiro: " + computerSelection);
+    if (playerSelection == computerSelection) {
+        console.log("Ha sido un empate por " + playerSelection);
+    } else if (playerSelection != computerSelection) {
+        switch (playerSelection) {
+            case "tijera":
+                if (computerSelection == "piedra") {
+                    return console.log("Tu pierdes... tijera pierde contra piedra");
+                } else {
+                    return console.log("Tu ganas! tijera gana a papel");
+                }
+                break;
+            case "papel":
+                if (computerSelection == "tijera") {
+                    return console.log("Tu pierdes... papel pierde contra tijera");
+                } else {
+                    return console.log("Tu ganas! papel gana a piedra");
+                }
+                break;
+            case "piedra":
+                if (computerSelection == "papel") {
+                    return console.log("Tu pierdes... piedra pierde contra papel");
+                } else {
+                    return console.log("Tu ganas! pidra gana a tijera");
+                }
+                break;
+            default:
+                return console.log("Esto ha sido un error");
+        }
+    }
+}
+
+gameplay(playerSelection(), computerPlay());
